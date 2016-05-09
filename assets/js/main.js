a1aab394bc20195f26be015641c3f88845a5c8e6
=hoanguyenxuan <=xuanhoa.redsand@gmail.com>
2016-05-09 08:09:30 +0700

    Initial HTML5 template

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+(function($) {
+	'use strict';
+
+	$(function() {
+
+		/* ==========================================================================
+	   		Demo Section Comment
+	    	========================================================================== */
+
+	}); // end document ready
+
+    $(window).resize(function(){
+
+    }); // end window resize
+
+})(jQuery) // end JQuery namespace
